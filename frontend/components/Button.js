96e5cefdd5227531646bb758b69e5ce800004e8a
Raphael Perez de Tagle <raphaeldetagle@gmail.com>
2019-03-07 18:56:49 -0800

    Added button

---
+++ frontend/components/Button.js
@@ -0,0 +1,14 @@
+import { React, Component } from 'react';
+import styled from 'styled-components';
+
+const Button = styled.button`
+  display: inline-block;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 20px;
+  display: block;
+`;
+
+export default Button;
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,7 +1,12 @@
+import React, { Component } from 'react';
+import Link from "next";
+import Button from "/components/button.js";
+
 const Index = () => (
     <div>
         <p>Home</p>
+        <Button>I love you</Button>
     </div>
-)
+);
 
 export default Index;
